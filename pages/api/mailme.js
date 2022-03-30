473699357851fa4c46d906735b882d38a4502d5b
marlopc <lucaspanaro1@gmail.com>
2022-03-29 22:29:09 -0300

    removes unused error variable

--- pages/api/mailme.js
+++ pages/api/mailme.js
@@ -21,7 +21,7 @@ const handler = async (req, res) => {
         : await transporter.sendMail(mail);
 
       res.status(200).send("SENT");
-    } catch (error) {
+    } catch {
       res.status(500).send("ERROR");
     }
   } else {
