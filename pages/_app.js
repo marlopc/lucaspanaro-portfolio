06ca951c2a6aab9184ae53df2826d20a486fba9c
marlopc <lucaspanaro1@gmail.com>
2022-05-06 19:03:11 -0300

    adds analytics, and font-face preload

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,61 @@
+import Head from "next/head";
+import Script from "next/script";
 import React from "react";
+import Layout from "../components/Layout";
 import "../public/assets/fonts/style.css";
 import "../styles/globals.css";
-import Layout from "../components/Layout";
+
+const AppHead = () => {
+  const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
+  return (
+    <Head>
+      {GOOGLE_ANALYTICS_ID && (
+        <>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          ></Script>
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', ${GOOGLE_ANALYTICS_ID})
+      `,
+            }}
+          ></Script>
+        </>
+      )}
+      <link
+        rel="preload"
+        href="/assets/fonts/Poppins-SemiBold.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href="/assets/fonts/SpaceMono-Regular.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin="anonymous"
+      />
+    </Head>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <AppHead />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
